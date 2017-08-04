4f3e7734321398db46665aa2ecad15741f9d3862
levonliu <levonliu@outlook.com>
2017-08-04 17:52:49 +0800

    'head'

--- src/components/header/header.vue
+++ src/components/header/header.vue
@@ -19,24 +19,62 @@
                     <span class="text">{{seller.supports[0].description}}</span>
                 </div>
             </div>
-            <div v-if="seller.supports" class="support-count">
+            <div v-if="seller.supports" class="support-count" @click="showDetail">
                 <span class="count">{{seller.supports.length}}个</span>
                 <i class="icon-keyboard_arrow_right"></i>
             </div>
         </div>
-        <div class="bulletin-wrapper"></div>
+        <div class="bulletin-wrapper" @click="showDetail">
+            <span class="bulletin-title"></span><span class="bulletin-text">{{seller.bulletin}}</span>
+            <i class="icon-keyboard_arrow_right"></i>
+        </div>
+        <div class="background">
+            <img :src="seller.avatar" width="100%" height="100%">
+        </div>
+        <div v-show="detailShow" class="detail">
+            <div class="detail-wrapper clearfix">
+                <div class="detail-main">
+                    <h1 class="name">{{seller.name}}</h1>
+                    <div class="star-wrapper">
+                        <star :size="48" :score="seller.score"></star>
+                    </div>
+                    <div class="title">
+                        <div class="line"></div>
+                        <div class="text">优惠信息</div>
+                        <div class="line"></div>
+                    </div>
+                </div>
+            </div>
+            <div class="detail-close">
+                <i class="icon-close"></i>
+            </div>
+        </div>
     </div>
 </template>
 
 <script type="text/ecmascript-6">
+    import star from '../star/star.vue';
     export default{
         props: {
           seller: {
             type: Object
           }
         },
+        data() {
+          return {
+            detailShow: false
+          };
+        },
+        methods: {
+          showDetail() {
+            this.detailShow = true;
+          }
+        },
         created() {
           this.classMap = ['decrease', 'discount', 'special', 'invoice', 'guarantee'];
+        },
+        components: {
+          star
         }
     };
 </script>
@@ -44,8 +82,10 @@
 <style lang="stylus" rel="stylesheet/stylus">
     @import "../../common/stylus/mixin.styl";
     .header
+        position relative
+        overflow hidden
         color #fff
-        background #999
+        background rgba(7, 17, 27, 0.5)
         .content-wrapper
             position relative
             padding 24px 12px 18px 24px
@@ -102,7 +142,7 @@
             .support-count
                 position absolute
                 right 12px
-                bottom 18px
+                bottom 14px
                 padding 0 8px
                 height 24px
                 line-height 24px
@@ -113,5 +153,85 @@
                     vertical-align top
                     font-size 10px
                 .icon-keyboard_arrow_right
+                    line-height 24px
+                    margin-left 2px
                     font-size 10px
+        .bulletin-wrapper
+            height 28px
+            line-height 28px
+            padding 0 22px 0 12px
+            white-space nowrap
+            overflow hidden
+            text-overflow ellipsis
+            position relative
+            background rgba(7, 17, 27, 0.2)
+            .bulletin-title
+                display inline-block
+                vertical-align top
+                margin-top 8px
+                width 22px
+                height 12px
+                bg-image('bulletin')
+                background-size 22px 12px
+                background-repeat no-repeat
+            .bulletin-text
+                font-size 10px
+                margin 0 4px
+            .icon-keyboard_arrow_right
+                position absolute
+                font-size 10px
+                right 12px
+                top 8px
+
+        .background
+            position absolute
+            top 0
+            left 0
+            width 100%
+            height 100%
+            z-index -1
+            filter blur(10px)
+        .detail
+            position fixed
+            z-index 100
+            top 0
+            left 0
+            width 100%
+            height 100%
+            overflow auto
+            background rgba(7, 17, 27, 0.8)
+            .detail-wrapper
+                width 100%
+                min-height 100%
+                .detail-main
+                    margin-top 64px
+                    padding-bottom 64px
+                    .name
+                        line-height 16px
+                        text-align center
+                        font-size 16px
+                        font-weight 700
+                    .star-wrapper
+                        margin-top 18px
+                        padding 2px 0
+                        text-align center
+                    .title
+                        display flex
+                        width 80%
+                        margin 30px auto 24px auto
+                        .line
+                            flex 1
+                            position relative
+                            top -6px
+                            border-bottom 1px solid rgba(255, 255, 255, 0.2)
+                        .text
+                            padding 0 12px
+                            font-size 14px
+            .detail-close
+                position relative
+                width 32px
+                height 32px
+                margin -64px auto 0 auto
+                clear both
+                font-size 32px
 </style>

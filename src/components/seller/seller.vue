8cb7ede4f14427833c3915f34d40d51a6a505c30
levonliu <levonliu@outlook.com>
2017-08-22 18:00:43 +0800

    'seller'

--- src/components/seller/seller.vue
+++ src/components/seller/seller.vue
@@ -131,7 +131,7 @@
 </script>
 
 <style lang="stylus" rel="stylesheet/stylus" type="text/stylus">
-    @import "../../common/stylus/mixin.styl"
+    @import "../../common/stylus/mixin.styl";
 
     .seller
         position: absolute
